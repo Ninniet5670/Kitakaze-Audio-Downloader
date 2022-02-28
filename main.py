e90d7adc8b65f9e00ba9ff74206bd96585745c5d
Ninniet5670 <marconeto3000@gmail.com>
2022-02-28 06:32:14 -0300

    telegram user id fixed xd

--- main.py
+++ main.py
@@ -13,21 +13,25 @@ bot = telebot.TeleBot(API_KEY)
 
 @bot.message_handler(commands=['start', 'help'])
 def responder(mensagem):
+    global USER_ID
+
     bot.reply_to(mensagem,
         '''
         /baixarmp3 Baixa seu vídeo em mp3
 
         ''')
+    USER_ID = mensagem.chat.id
 
 
 @bot.message_handler(commands=['baixarmp3'])
 def baixar_mp3(mensagem):
+    global DOWNLOADABLE
+    
     bot.reply_to(mensagem, 
         '''
         Envie seu link válido do YouTube    
             
         ''')
-    global DOWNLOADABLE
     DOWNLOADABLE = True
 
 
@@ -40,6 +44,9 @@ def downloadable(self):
 
 @bot.message_handler(func=downloadable)
 def download_mp3(link):
+    global USER_ID
+    global DOWNLOADABLE
+
     try:
         video_url = link.text
         video_info = youtube_dl.YoutubeDL().extract_info(
@@ -56,17 +63,15 @@ def download_mp3(link):
             ydl.download([video_info['webpage_url']])
 
         with open(filename, 'rb') as audio_file:
-            bot.send_audio('1095525330', audio_file)
+            bot.send_audio(USER_ID, audio_file)
 
         os.remove(filename)
-
         print("Download complete... {}".format(filename))
 
-        global DOWNLOADABLE
         DOWNLOADABLE = False
 
     except:
-        bot.send_message('1095525330', 'Não foi possível concluir a solicitação de seu envio, tente novamente')
+        bot.send_message(USER_ID, 'Não foi possível concluir a solicitação de seu envio, tente novamente')
 
 
 bot.polling()
