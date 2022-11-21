88098ca485423ef10625bb2cc647a857ede2dbc7
Ninniet5670 <marconeto3000@gmail.com>
2022-11-20 23:59:26 -0300

    poo initiated

--- main.py
+++ main.py
@@ -5,93 +5,68 @@ import youtube_dl
 
 load_dotenv()
 
-API_KEY = os.getenv('API_KEY')
-bot = telebot.TeleBot(API_KEY)
-USER_ID = ''
-
 DOWNLOADABLE = False
 
-class Telegram_bot:
-    def __init__(self, bot, user_id):
-        self.bot = bot
-        self.user_id = user_id
-
-
-    # @bot.message_handler(commands=['start', 'help'])
-    # def responder(self, mensagem):
-    #     global USER_ID
-
-    #     self.bot.reply_to(mensagem,
-    #         '''
-    #         /baixarmp3 Baixa seu vídeo em mp3
-
-    #         ''')
-    #     USER_ID = mensagem.chat.id
-
-
-    # @bot.message_handler(commands=['baixarmp3'])
-    def baixar_mp3(self, mensagem):
-        global DOWNLOADABLE
-        
-        self.bot.reply_to(mensagem, 
-            '''
-            Envie seu link válido do YouTube    
-                
-            ''')
-        DOWNLOADABLE = True
-
-
-    def downloadable(self):
-        if DOWNLOADABLE:
-            return True
-        else:
-            return False
-
-
-    # @bot.message_handler(func=downloadable)
-    def download_mp3(self, link):
-        global USER_ID
-        global DOWNLOADABLE
-
-        try:
-            video_url = link.text
-            video_info = youtube_dl.YoutubeDL().extract_info(
-                url = video_url,download=False
-            )
-            filename = f"{video_info['title']}.mp3"
-            options={
-                'format':'bestaudio/best',
-                'keepvideo':False,
-                'outtmpl':filename,
-            }
-
-            with youtube_dl.YoutubeDL(options) as ydl:
-                ydl.download([video_info['webpage_url']])
-
-            with open(filename, 'rb') as audio_file:
-                self.bot.send_audio(USER_ID, audio_file)
-
-            os.remove(filename)
-            print("Download complete... {}".format(filename))
-
-            DOWNLOADABLE = False
-
-        except:
-            self.bot.send_message(USER_ID, 'Não foi possível concluir a solicitação de seu envio, tente novamente')
+bot = telebot.TeleBot(os.getenv('API_KEY'))
 
 
 @bot.message_handler(commands=['start', 'help'])
-def responder(self, mensagem):
-        global USER_ID
+def responder(mensagem):
+    global USER_ID
 
-        self.bot.reply_to(mensagem,
-            '''
-            /baixarmp3 Baixa seu vídeo em mp3
-
-            ''')
-        USER_ID = mensagem.chat.id
+    bot.reply_to(mensagem, '''/baixarmp3 Baixa seu vídeo em mp3''')
+    USER_ID = mensagem.chat.id
 
 
-Telegram_bot(bot, USER_ID)
+@bot.message_handler(commands=['baixarmp3'])
+def baixar_mp3(mensagem):
+    global DOWNLOADABLE
+    
+    bot.reply_to(mensagem, 
+        '''
+        Envie seu link válido do YouTube    
+            
+        ''')
+    DOWNLOADABLE = True
+
+
+def downloadable():
+    if DOWNLOADABLE:
+        return True
+    else:
+        return False
+
+
+@bot.message_handler(func=downloadable)
+def download_mp3(link):
+    global USER_ID
+    global DOWNLOADABLE
+
+    try:
+        video_url = link.text
+        video_info = youtube_dl.YoutubeDL().extract_info(
+            url = video_url,download=False
+        )
+        filename = f"{video_info['title']}.mp3"
+        options={
+            'format':'bestaudio/best',
+            'keepvideo':False,
+            'outtmpl':filename,
+        }
+
+        with youtube_dl.YoutubeDL(options) as ydl:
+            ydl.download([video_info['webpage_url']])
+
+        with open(filename, 'rb') as audio_file:
+            bot.send_audio(USER_ID, audio_file)
+
+        os.remove(filename)
+        print("Download complete... {}".format(filename))
+
+        DOWNLOADABLE = False
+
+    except:
+        bot.send_message(USER_ID, 'Não foi possível concluir a solicitação de seu envio, tente novamente')
+
 
 bot.polling()
